9d0997526cba7d0cc67e530849d015a1e097f682
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-04-03 14:32:34 +0530

    feat(outputs): expose env resource names plus dev cluster and ETL job IDs for downstream integrations

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -1,23 +1,44 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.main.name
+  value       = azurerm_resource_group.main.name
+  description = "Deployed resource group name"
 }
 
 output "storage_account_name" {
-  value = module.storage.storage_account_name
+  value       = module.storage.storage_account_name
+  description = "ADLS Gen2 storage account name"
 }
 
 output "databricks_workspace_url" {
-  value = module.databricks.workspace_url
-}
-
-output "unity_catalog_name" {
-  value = module.databricks_unity.catalog_name
+  value       = module.databricks.workspace_url
+  description = "Databricks workspace URL"
 }
 
 output "key_vault_name" {
-  value = module.keyvault.key_vault_name
+  value       = module.naming.names.kv
+  description = "Key Vault name"
 }
 
 output "adf_name" {
-  value = module.adf.adf_name
+  value       = module.naming.names.adf
+  description = "ADF instance name"
+}
+
+output "log_analytics_workspace_id" {
+  value       = module.monitoring.log_analytics_workspace_id
+  description = "Log Analytics workspace ID"
+}
+
+output "unity_catalog_name" {
+  value       = "${var.project}_${var.environment}"
+  description = "Unity Catalog catalog name"
+}
+
+output "dev_cluster_id" {
+  value       = module.databricks_unity.dev_cluster_id
+  description = "Dev single-node cluster ID (null for staging/prod)"
+}
+
+output "etl_job_id" {
+  value       = module.databricks_unity.etl_job_id
+  description = "ETL job ID (null for dev)"
 }
